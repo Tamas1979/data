7cc119b7d6e11a76b4f31649098ac0c932a9b4d2
Tamas1979 <32540111+Tamas1979@users.noreply.github.com>
2023-04-08 14:34:57 +0200

    Create send_v2.vue

---
+++ send_v2.vue
@@ -0,0 +1,19 @@
+try {
+
+			const response = await fetch('http://127.0.0.1:8000/api/recipe', {
+				method: 'POST',
+				body: formData,
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json',
+				},
+				body: JSON.stringify({
+					file: imageFile.value,
+					name: recipe.value.name,
+					description: recipe.value.description,
+					recipeLevel: recipe.value.recipeLevel,
+					preparationTime: recipe.value.preparationTime,
+					cookingTime: recipe.value.cookingTime,
+					steps: recipe.value.steps,
+				})
+			})
